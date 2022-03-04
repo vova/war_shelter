c430b6bd883a84a0eb32171a7531a7bb23f73c24
VC085483 <Volodymyr_Chernikov@epam.com>
2022-03-04 14:17:05 +0200

    Remove butter cms configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-
-  scope :module => 'buttercms' do
-    get '/categories/:slug' => 'categories#show', :as => :buttercms_category
-    get '/author/:slug' => 'authors#show', :as => :buttercms_author
-
-    get '/blog/rss' => 'feeds#rss', :format => 'rss', :as => :buttercms_blog_rss
-    get '/blog/atom' => 'feeds#atom', :format => 'atom', :as => :buttercms_blog_atom
-    get '/blog/sitemap.xml' => 'feeds#sitemap', :format => 'xml', :as => :buttercms_blog_sitemap
-
-    get '/blog(/page/:page)' => 'posts#index', :defaults => {:page => 1}, :as => :buttercms_blog
-    get '/blog/:slug' => 'posts#show', :as => :buttercms_post
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
