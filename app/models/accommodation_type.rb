f2d4d49fe77f259196e687d65028185f254d66eb
Andriy Savka <savka.ai2015@gmail.com>
2022-03-04 14:30:17 +0200

    Update app/models/accommodation_type.rb
    Co-authored-by: s-nestor <100867355+s-nestor@users.noreply.github.com>

--- app/models/accommodation_type.rb
+++ app/models/accommodation_type.rb
@@ -1,7 +1,7 @@
 class AccommodationType < ApplicationRecord
   ACCOMMODATION_TyPES = %i[shelter hostel hotel private_house friendly_apartment rent_apartment]
 
-  enum name: ACCOMMODATION_TyPES
+  enum name: ACCOMMODATION_TYPES
 
   validates_presence_of :name
 end
