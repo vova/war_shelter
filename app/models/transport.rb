d5dbdf43969bfd6dfe321c39ff0e773b3039f950
Oleksandr Kychun <oleksandr.kychun@visiquate.com>
2022-03-05 16:22:20 +0200

    issue 2: add users dashboard to active admin: creation support, show, index and filters

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,4 +1,7 @@
 class Transport < ApplicationRecord
+  BY_COMPANY = 'Provided by company'
+  NOT_BY_COMPANY = 'Not provided by company'
+
   enum name: {
     car: 'Car',
     train: 'Train',
@@ -6,4 +9,18 @@ class Transport < ApplicationRecord
     no_transport: 'No transport',
     unknown: 'Unknown'
   }, _default: 'Car'
+
+  def self.to_select_collection
+    all.map do |tr|
+      [
+        tr.presentable_name,
+        tr.id
+      ]
+    end
+  end
+
+  def presentable_name
+    "#{name} - "\
+    "#{company_transfer ? BY_COMPANY : NOT_BY_COMPANY }"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :user_status,
+             foreign_key: 'status_id', class_name: 'UserStatus'
+  belongs_to :coordinator,
+             foreign_key: 'coordinator_id', class_name: 'AdminUser'
+  belongs_to :accommodation_type,
+             foreign_key: 'accommodation_pref', class_name: 'AccommodationType'
+  belongs_to :transport
 end
