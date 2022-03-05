a9431f20763b7268e0b2f9d1a13294896a894517
alex-nestor <nestorisena2001@gmail.com>
2022-03-05 22:50:40 +0200

    fix user factory according to new associations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email { FFaker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
-    status_id { 1 }
-    coordinator_id { 1 }
+    status_id { create(:user_status).id }
+    coordinator_id { create(:admin_user).id }
     from { FFaker::Address.city }
     destination { FFaker::Address.city }
     adults { 2 }
@@ -15,8 +15,8 @@ FactoryBot.define do
     phone { FFaker::PhoneNumber.phone_number }
     phone2 { FFaker::PhoneNumber.phone_number }
     geo { 'UA' }
-    accommodation_pref { 3 }
-    transport_id { 1 }
+    accommodation_pref { create(:accommodation_type).id }
+    transport_id { create(:transport).id }
     date_arrival { Time.now + 2.days }
     request_id { '2' }
     vaccination { true }
