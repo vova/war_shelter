c1bc2d85f0e5394328525748b57a62695fa11677
alex-nestor <nestorisena2001@gmail.com>
2022-03-05 22:51:46 +0200

    add phone, name and vaccination to AdminUser model

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'table columns' do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to have_db_column(:phone).of_type(:string) }
+    it { is_expected.to have_db_column(:phone2).of_type(:string) }
+    it { is_expected.to have_db_column(:vaccination).of_type(:boolean) }
+  end
 end
