4c27217a3f3a54904f1d004ec951fce358b29901
Oleksandr Kychun <ghostinshadow@users.noreply.github.com>
2022-03-07 20:36:09 +0200

    issue 59: integrate pg_search for places (#63)
    * issue 59: integrate pg_search for places
    
    * issue 59: add super search to admin page
    
    Co-authored-by: Oleksandr Kychun <oleksandr.kychun@visiquate.com>

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   include PhoneValidations
+  include PgSearch::Model
 
   belongs_to :user, inverse_of: :places, touch: true,
              foreign_key: 'assigned_to', class_name: 'User'
@@ -21,4 +22,44 @@ class Place < ApplicationRecord
   }, _default: 'UAH', _prefix: :currency
 
   scope :to_pay_attention, lambda { |current_admin_user| where(coordinator_id: current_admin_user.id).where.not(status: :not_available) }
+
+  TSEARCH_WEIGHTS = {
+    highest: 'A',
+    important: 'B',
+    low: 'C'
+  }.freeze
+
+  SUPER_SEARCH_CONFIG = {
+    against: [
+      [:name, TSEARCH_WEIGHTS[:highest]],
+      [:city, TSEARCH_WEIGHTS[:highest]],
+      [:region, TSEARCH_WEIGHTS[:important]],
+      [:address, TSEARCH_WEIGHTS[:highest]],
+      [:phone, TSEARCH_WEIGHTS[:important]],
+      [:phone2, TSEARCH_WEIGHTS[:low]],
+      [:contact_name, TSEARCH_WEIGHTS[:important]],
+      [:comment, TSEARCH_WEIGHTS[:important]]
+    ],
+    associated_against: {
+      user: :name,
+      coordinator: :name,
+      accommodation_type: :name
+    },
+    using: {
+      tsearch: {
+        prefix: true,
+        any_word: true
+      },
+      trigram: { threshold: 0.2 }
+    },
+    ranked_by: ':trigram'
+  }.freeze
+
+  pg_search_scope :super_search, SUPER_SEARCH_CONFIG
+
+  scope :content_matches, ->(term) { super_search(term) }
+
+  def self.ransackable_scopes(_auth_object = nil)
+    %i[content_matches]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_074823) do
+ActiveRecord::Schema.define(version: 2022_03_07_150814) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "accommodation_types", force: :cascade do |t|
