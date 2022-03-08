72317330e0f28523f53793ef8a47c6150533365c
Oleksandr Kychun <oleksandr.kychun@visiquate.com>
2022-03-08 13:39:54 +0200

    issue 56: basic setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,7 @@ set :repo_url, "git@github.com:vova/war_shelter.git"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
