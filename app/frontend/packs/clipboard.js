7bbf8c4d6d8c56c9a9679966500db48b4c7ae63c
yuramon <yuramon2405@gmail.com>
2022-03-08 03:00:01 +0200

    minor changes

--- app/frontend/packs/clipboard.js
+++ app/frontend/packs/clipboard.js
@@ -8,14 +8,12 @@ $(document).ready(function(){
 
         const currentLabel = button.innerHTML;
 
-        // Exit label update when already in progress
         if(button.innerHTML === 'Copied!'){
             return;
         }
 
         button.innerHTML = 'Copied!';
 
-        // Revert button label after 3 seconds
         setTimeout(function(){
             button.innerHTML = currentLabel;
         }, 3000)
