d94ec220c2abf7cb2a64b2f1aec2c70376dec4e2
Oleksandr Kychun <oleksandr.kychun@visiquate.com>
2022-03-08 18:47:59 +0200

    issue 69: basic structure for users home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+
+  def index
+    @available_places = Place.available_places_for(
+      current_user
+    ).includes(
+      :coordinator, :accommodation_type, :user
+    )
+  end
 end
