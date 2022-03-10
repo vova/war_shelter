94102917011c8d53fddf29910589c7d71e9e20ca
Oleksandr Kychun <oleksandr.kychun@visiquate.com>
2022-03-10 13:53:03 +0200

    issue 74: beautify available places page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  get 'places/:id', to: 'home#show', as: :place
   ActiveAdmin.routes(self)
   devise_for :users
   root to: "home#index"
