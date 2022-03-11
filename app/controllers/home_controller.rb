76724c455b82c05a2c2d992c8ad3aeb3cbc5f620
transfer76 <44622576+transfer76@users.noreply.github.com>
2022-03-11 18:38:29 +0200

    Add show for home page places (#79)
    * Add show for home page places
    
    * Update home page show
    
    * Add check specific available place for current usere

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_available_places, only: %i[index show]
 
-  def index
+  def index; end
+
+  def show
+    if @available_places.pluck(:id).include?(params[:id].to_i)
+      @place = Place.find(params[:id])
+    else
+      flash.now.alert = I18n.t('place_is_not_available')
+      render :index
+    end
+  end
+
+  private
+
+  def set_available_places
     @available_places = Place.available_places_for(
       current_user
     ).includes(
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  get 'places/:id', to: 'home#show', as: :place
-  ActiveAdmin.routes(self)
   devise_for :users
+
+  ActiveAdmin.routes(self)
   root to: "home#index"
+
+  get 'places/:id', to: 'home#show', as: :place
+
   resources :users, only: %i[show edit update]
 end
