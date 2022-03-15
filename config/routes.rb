2ec1e254a46cfa1561ab226cce36e021e4591de8
s-nestor <100867355+s-nestor@users.noreply.github.com>
2022-03-15 15:39:03 +0300

    Add change password page (#85)
    * beautify change password page
    
    * remove unnecessary buttons on change password page
    
    * remove unnecessary template
    
    Co-authored-by: alex-nestor <nestorisena2001@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: "home#index"
 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'registration'
+  end
+
   get 'places/:id', to: 'home#show', as: :place
 
   resources :users, only: %i[show edit update]
